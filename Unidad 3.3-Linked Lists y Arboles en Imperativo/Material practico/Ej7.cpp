c41eb9a87085971edca7d1477c8c01f8f56d3f90
usuario publico <publico@ALU.UNQ.EDU.AR>
2022-06-23 19:38:17 -0300

    avance arboles

--- Unidad 3.3-Linked Lists y Arboles en Imperativo/Material practico/Ej7.cpp
+++ Unidad 3.3-Linked Lists y Arboles en Imperativo/Material practico/Ej7.cpp
@@ -12,20 +12,56 @@ int sumarT(Tree t) {
 }
 
 // 2. Dado un árbol binario devuelve su cantidad de elementos, es decir, el tamaño del árbol (size en inglés).
-int sizeT(Tree t)
+int sizeT(Tree t) {
+    if(t == NULL) {
+        return 0;
+    } else {
+        return 1 + sizeT(left(t)) + sizeT(right(t));
+    }
+}
+
+// 3. Dados un elemento y un árbol binario devuelve True si existe un elemento igual a ese en el árbol.
+bool perteneceT(int e, Tree t) {
+   if(!isEmptyT(t)){
+        return e == rootT(t) || perteneceT(e, left(t)) || perteneceT(e, right(t));
+   } else {
+        return false;
+   }
+}
+
+// 4. Dados un elemento e y un árbol binario devuelve la cantidad de elementos del árbol que son
+// iguales a e.
+int aparicionesT(int e, Tree t) {
+    int c = 0;
+    if(!isEmptyT(t)){
+        if(e == rootT(t)){
+            c++;    
+        }
+        return c + aparicionesT(e, left(t)) + aparicionesT(e, right(t)); 
+    }
+    return c;
+}
+
+// 5. Dado un árbol devuelve su altura.
+int heightT(Tree t) {
+    // sumar 1 + el maximo entre la altura de left y right cuando no es vacio
+    // cuando es vacio devolver 0
+    return 0;
+}
+
+
 
 
 
 
 int main() {
-    Tree empty = emptyT();
+    Tree left = nodeT(2, nodeT(5, NULL, NULL), nodeT(4, NULL, NULL));
 
-    Tree left = nodeT(2, nodeT(3, empty, empty), nodeT(4, empty, empty));
-
-    Tree right = nodeT(5, nodeT(6, empty, empty), nodeT(7, empty, empty));
+    Tree right = nodeT(5, nodeT(6, NULL, NULL), nodeT(7, nodeT(5, NULL, NULL), NULL));
     
     Tree t = nodeT(1, left, right);
 
-    cout << "Suma del arbol: " << sumarT(t) << endl;    
+    //cout << "Cantidad de elementos: " << sizeT(t) << endl;    
+    cout << "Apariciones 5: " << aparicionesT(5,t) << endl;    
 
 }
