9a86ad900718c0bc0c9d9086f7cdf42f8a8ec1ec
usuario publico <publico@ALU.UNQ.EDU.AR>
2022-06-28 19:42:11 -0300

    sigue tp

--- TP de C/2022s1-TP-Codigo-Provisto/Img.cpp
+++ TP de C/2022s1-TP-Codigo-Provisto/Img.cpp
@@ -5,6 +5,8 @@
 #define HORIZONTAL 42
 #define VERTICAL   17
 
+#include "Matrix.h" 
+
 struct ITreeSt {     
     DIR      division;    
     Color    color;       
@@ -21,7 +23,10 @@ struct ITreeSt {
 
 struct ImgSt {
     // COMPLETAR
-    
+    int width;
+    int height;
+    int size;
+    ITreeSt* itree;
 };
  /* INV.REP.
     // COMPLETAR
@@ -32,7 +37,9 @@ struct ImgSt {
 //---------------------------------------------------------
 int sizeImg(Img img) {
     // COMPLETAR
+    return img->size;
 }
+
 
 //---------------------------------------------------------
 // createImg
@@ -41,11 +48,47 @@ int sizeImg(Img img) {
 ITreeSt* loadIT(int iw, int ih
                ,int fw, int fh
                ,int n, Matrix m, DIR d) {
+    ITreeSt* itree = new ITreeSt;
+    if(n == 1) {
+      itree->division = HOJA;
+      itree->color = M_getAt(m, iw, ih);
+      itree->first = NULL;
+      itree->second = NULL;
+      return itree;
+    } else if(n != 1 && d == HORIZONTAL) {
+      itree->division = HORIZONTAL;
+      itree->first = loadIT(iw, ih, fw, (fh/2), (n/2), m, VERTICAL);
+      itree->second = loadIT(iw, (ih+fh/2), fw, (fh/2), (n/2), m, VERTICAL);
+      return itree;
+    } else if(n != 1 && d == VERTICAL) {
+      itree->division = VERTICAL;
+      itree->first = loadIT(iw, ih, (fw/2), (fh), (n/2), m, HORIZONTAL);
+      itree->second = loadIT((iw+fw/2), ih, (fw/2), fh, (n/2), m, HORIZONTAL);
+      return itree;
+    }
+  
+}
+
+int nextPow2(int n) {
+    
+    if() { 
+      return 
+    }
 }
 
 // PRECOND: w es potencia de 2, m es de w*w
 Img createImg(Matrix m, int w) {
   // COMPLETAR
+  int ch = M_width(m);
+  int cw = M_height(m);
+  int h = nextPow2(ch);
+  int w = nextPow2(cw);
+  int n = max(w, h);
+  
+
+
+  // aca tengo que devolver una img 
+  return 
 }
 
 //---------------------------------------------------------
